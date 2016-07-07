d7571964cf378eb162bad2a474e4af25d7ad271d
zhangjk4859 <573581325@qq.com>
2016-07-07 18:01:16 +0800

    初始化了工程

---
+++ shanbayWords/shanbayWords/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  shanbayWords
+//
+//  Created by 张俊凯 on 16/7/7.
+//  Copyright © 2016年 张俊凯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+    }
+
+
+
+}
+

a5af440c711588a58890452366b0d9567e3cd982
zhangjk4859 <573581325@qq.com>
2016-07-31 21:40:15 +0800

    测试懒加载

--- ShanbayWords/ShanbayWords/扇贝单词/JKWordsVC.swift
+++ ShanbayWords/ShanbayWords/扇贝单词/JKWordsVC.swift
@@ -10,45 +10,43 @@ import UIKit
 
 class JKWordsVC: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         tabBarItem.title = "扇贝单词"
-        //view.backgroundColor = UIColor.redColor()
         
         //添加标题栏
-        let titleView = UIView()
-        titleView.frame = CGRect(x: 0, y: 64, width: view.frame.size.width, height:30)
         view.addSubview(titleView)
-        titleView.backgroundColor = UIColor.purpleColor()
         
-        //添加四个按钮
         
-        let width = view.frame.size.width / 4
-        for index in 0...3{
-            let btn = UIButton()
-            
-            let btnX = index * width
-            
-            btn.frame = CGRect(x: btnX , y: 0, width: width, height: 30)
-            
-        }
+//        //添加四个按钮
+//        
+//        let width = view.frame.size.width / 4
+//        for index in 0...3{
+//            let btn = UIButton()
+//            
+//            let btnX = index * width
+//            
+//            btn.frame = CGRect(x: btnX , y: 0, width: width, height: 30)
+//            
+//        }
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //设置tableView
+        
+        
+        
+        
+        
     }
     
+    
+    //懒加载
+    private lazy var titleView :UIView = {
+        let titleView = UIView()
+        titleView.frame = CGRect(x: 0, y: 64, width: UIScreen.mainScreen().bounds.size.width, height:30)
+        titleView.backgroundColor = UIColor.purpleColor()
+        return titleView
+        }()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
